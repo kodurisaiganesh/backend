61179873deda700094e8311dc8f51129073eb5f8
kodurisaiganesh <146118669+kodurisaiganesh@users.noreply.github.com>
2025-06-15 11:37:26 +0530

    serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,20 +3,22 @@ from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Blog
 
+# ✅ Include full author info
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
 class BlogSerializer(serializers.ModelSerializer):
-    author_username = serializers.SerializerMethodField()
+    author = AuthorSerializer(read_only=True)  # ✅ Full author object with id and username
 
     class Meta:
         model = Blog
         fields = [
             'id', 'title', 'slug', 'content', 'created_at', 'updated_at',
-            'is_published', 'author', 'author_username'
+            'is_published', 'author'
         ]
-        read_only_fields = ['author', 'slug', 'created_at', 'updated_at', 'author_username']
-
-    def get_author_username(self, obj):
-        return obj.author.username
-
+        read_only_fields = ['author', 'slug', 'created_at', 'updated_at']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -32,11 +34,11 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
 
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
         token['username'] = user.username
         token['email'] = user.email
+        token['user_id'] = user.id  # ✅ Add user ID for easier use in frontend
         return token
