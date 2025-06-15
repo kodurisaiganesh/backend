fb60cacfe4ae29c8a37f2f33ca8f7d1e21432688
kodurisaiganesh <146118669+kodurisaiganesh@users.noreply.github.com>
2025-06-15 12:24:40 +0530

    views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from django.http import JsonResponse  # ✅ For health_check
+from rest_framework.exceptions import PermissionDenied
+from django.http import JsonResponse
 from .models import Blog
 from .serializers import BlogSerializer
 
@@ -11,37 +12,35 @@ def health_check(request):
     return JsonResponse({"status": "ok"})
 
 
+# ✅ List and Create View
 class BlogListCreateAPIView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['request'] = self.request
-        return context
+        return {'request': self.request}
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
+# ✅ Retrieve, Update, and Destroy View with Author Permissions
 class BlogRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['request'] = self.request
-        return context
+        return {'request': self.request}
 
     def perform_update(self, serializer):
         blog = self.get_object()
         if blog.author != self.request.user:
-            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
+            raise PermissionDenied("You are not the author of this blog.")
         serializer.save()
 
     def perform_destroy(self, instance):
         if instance.author != self.request.user:
-            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
+            raise PermissionDenied("You are not the author of this blog.")
         instance.delete()
